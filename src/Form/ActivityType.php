ff5ec7123483a00dd7d7980c05b8d29d6d47618d
Blizz <theowincke@live.fr>
2024-09-04 16:21:32 +0200

    fix bug

--- src/Form/ActivityType.php
+++ src/Form/ActivityType.php
@@ -12,7 +12,6 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
