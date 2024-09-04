bba1f984135121562b3ccd1415e4260d8580c7a6
Maxime HEIM <maxime.heim2023@campus-eni.fr>
2024-09-04 15:21:29 +0200

    conflits

--- src/Form/ActivityType.php
+++ src/Form/ActivityType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
