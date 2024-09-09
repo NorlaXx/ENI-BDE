6433e7cee78efe152d77398f4bf08ce47231a8e2
alexis-rochefort <alexis.rochefort35@gmail.com>
2024-09-09 10:32:23 +0200

    ✨Affichage le nombre de filtres sélectionnés

---
+++ public/scripts/filter.js
@@ -0,0 +1,33 @@
+const subtitle = document.querySelector('.content .filter h2');
+
+function getNbFilter(){
+    document.querySelector('.content .filter form').addEventListener('submit', (e) => {
+        const inputs = document.querySelectorAll('input, select');
+
+        let filledFieldsCount = 0;
+
+        inputs.forEach(input => {
+            // Pour les champs de type 'text', 'email', 'number', et les 'select'
+            if ((input.type === 'text' || input.type === 'email' || input.type === 'number' || input.tagName.toLowerCase() === 'select') && input.value) {
+                filledFieldsCount++;
+            }
+
+            // Pour les champs de type 'checkbox' et 'radio'
+            if ((input.type === 'checkbox' || input.type === 'radio') && input.checked) {
+                filledFieldsCount++;
+            }
+        })
+        localStorage.setItem('filledFieldsCount', filledFieldsCount);
+    })
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const filledFieldsCount = localStorage.getItem('filledFieldsCount');
+
+        if (filledFieldsCount !== null) {
+            subtitle.innerHTML = '';
+            subtitle.innerHTML = `Filtres (${filledFieldsCount})`;
+        }
+    });
+}
+
+getNbFilter();
