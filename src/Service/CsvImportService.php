c21655413600995512d64582c1ee410b116ee2e5
Maxime HEIM <maxime.heim2023@campus-eni.fr>
2024-09-10 13:39:09 +0200

    add form for csv import

--- src/Service/CsvImportService.php
+++ src/Service/CsvImportService.php
@@ -63,46 +63,39 @@ class CsvImportService extends AbstractController
         $lastName = $data[4] ?? '';
         $firstName = $data[5] ?? '';
 
-//        dd($email);
-
-        // Check if user already exists
-        $existingUser = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
-        if ($existingUser) {
-            // Handle duplicate email error
-            $this->addFlash('error', 'Tous les emails doivent être uniques !');
-            $this->redirectToRoute("app_profil_list");
-        }
-        try {
-            $user = new User();
+        $user = new User();
+        if($this->entityManager->getRepository(User::class)->findOneBy(['email' => $email])){
+            $this->addFlash('error', 'Tous les emails doivent être uniques!');
+            $this->redirectToRoute("app_profil_liste");
+        } else {
             $user->setEmail($email);
-            $user->setRoles(["ROLE_USER"]);
-
-            // Set "password" to default password for new User
-            $hashedPassword = $this->passwordHasher->hashPassword($user, "password");
-            $user->setPassword($hashedPassword);
-            $user->setPhoneNumber($phoneNumber);
-            $user->setPseudo($pseudo);
-
-        } catch (\Exception $e) {
-            dd($e->getMessage());
         }
-        try {
-            // Find campus or set default to Rennes
-            $campus = $this->entityManager->getRepository(Campus::class)->findOneBy(['name' => $campusName]);
-            if (!$campus) {
-                $campus = $this->entityManager->getRepository(Campus::class)->findOneBy(['name' => "Rennes"]);
-            }
-            $user->setCampus($campus);
 
-            $user->setFileName("default.jpg");
-            $user->setActive(true);
-            $user->setLastName($lastName);
-            $user->setFirstName($firstName);
 
-            $this->userRepository->update($user);
-        } catch (\Exception $e) {
-            dd($e->getMessage());
+        $user->setRoles(["ROLE_USER"]);
+//        Set "password" to default password for new User
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $user,
+            "password"
+        );
+        $user->setPassword($hashedPassword);
+        $user->setPhoneNumber($phoneNumber);
+        $user->setPseudo($pseudo);
+
+        $campus = $this->entityManager->getRepository(Campus::class)->findOneBy(['name' => $campusName]);
+
+//        Si le nom du campus n'est pas connue, valuer set a Rennes par defaut
+        if (!$campus) {
+             $campus = $this->entityManager->getRepository(Campus::class)->findOneBy(['name' => "Rennes"]);
         }
+        $user->setCampus($campus);
+
+        $user->setFileName("default.jpg");
+        $user->setActive(true);
+        $user->setLastName($lastName);
+        $user->setFirstName($firstName);
+
+        $this->entityManager->persist($user);
     }
 
 }
