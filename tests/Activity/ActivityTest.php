b91a4c46374d5ca8738b84d6d9b29eeb8b1fc5cf
alexis-rochefort <alexis.rochefort35@gmail.com>
2024-09-11 14:07:57 +0200

    ✅tests création sortie

--- tests/Activity/ActivityTest.php
+++ tests/Activity/ActivityTest.php
@@ -3,6 +3,10 @@
 namespace App\Tests\Activity;
 
 use App\Entity\Activity;
+use App\Entity\ActivityState;
+use App\Entity\Campus;
+use App\Entity\Lieu;
+use App\Entity\User;
 use App\Repository\ActivityRepository;
 use PHPUnit\Framework\TestCase;
 
@@ -19,23 +23,62 @@ class ActivityTest extends TestCase
 
     public function testFindAll(): void
     {
+        $campus = new Campus();
+        $campus->setName('ENI - Rennes');
+        $campus->setPostalCode('35131');
+        $campus->setLongitude(1.234567);
+        $campus->setLatitude(1.234567);
+        $campus->setAddress('8 rue Léo Lagrange');
+        $campus->setFileName('file.jpg');
+        $campus->setNblimitPlaces(10);
+        $campus->setCity('Chartres de bretagne');
+
+        $lieu = new Lieu();
+        $lieu->setName('picine de Bréquiny');
+        $lieu->setLongitude(1.234567);
+        $lieu->setLatitude(1.234567);
+        $lieu->setAddress('12 Bd Albert 1er');
+        $lieu->setPostalCode('35000');
+        $lieu->setCity('Rennes');
+        $lieu->setFileName('file.jpg');
+
+        $user = new User();
+        $user->setActive(true);
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword('password');
+        $user->setEmail('email@email.com');
+        $user->setFirstName('John');
+        $user->setLastName('Doe');
+        $user->setPhoneNumber('0606060606');
+        $user->setPseudo('johndoe');
+        $user->setCampus($campus);
+        $user->setFileName('file.jpg');
+
+        $activityState = new ActivityState();
+        $activityState->setCode('ACT_CR');
+        $activityState->setWording('Créée');
+
         $activity1 = new Activity();
         $activity1->setName('Activity 1');
         $activity1->setDescription('Description 1');
-
-        $activity2 = new Activity();
-        $activity2->setName('Activity 2');
-        $activity2->setDescription('Description 2');
+        $activity1->setDuration(40);
+        $activity1->setCampus($campus);
+        $activity1->setLieu($lieu);
+        $activity1->setOrganizer($user);
+        $activity1->setFileName('file.jpg');
+        $activity1->setNbLimitParticipants(10);
+        $activity1->setRegistrationDateLimit(new \DateTime('2024-09-30 23:59:00'));
+        $activity1->setStartDate(new \DateTime('2024-10-01 14:00:00'));
+        $activity1->setCreationDate(new \DateTime());
+        $activity1->setState($activityState);
 
         $activityRepository = $this->createMock(ActivityRepository::class);
         $activityRepository->method('findAll')
-            ->willReturn([$activity1, $activity2]);
+            ->willReturn([$activity1]);
 
         $activities = $activityRepository->findAll();
 
         $this->assertEquals('Activity 1', $activities[0]->getName());
         $this->assertEquals('Description 1', $activities[0]->getDescription());
-        $this->assertEquals('Activity 2', $activities[1]->getName());
-        $this->assertEquals('Description 2', $activities[1]->getDescription());
     }
 }
